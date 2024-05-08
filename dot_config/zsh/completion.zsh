46d935de750cc71e0c019091ca12f69b9b0f77ae
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-08 14:18:48 +0200

    improve zsh completion

--- dot_config/zsh/completion.zsh
+++ dot_config/zsh/completion.zsh
@@ -1,35 +1,5 @@
-autoload -U compinit; compinit
-
-# use completion cache
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 
-# allow completion menu to be selectable
-zstyle ':completion:*' menu select
-
-# complete command options
-zstyle ':completion:*' complete-options true
-
-# define completers
-zstyle ':completion:*' completer _extensions _complete _approximate
-
-# try case-sensitive match, if nothing matches try case-insensitive.
-# also applies to sub-strings
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
-
-# formatting the menu
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-
-# Required for completion to be in good groups (named after the tags)
-zstyle ':completion:*' group-name ''
-
-
-# reordering listed groups
-zstyle ':completion:*:*:-command-:*:*' group-order aliases commands builtins
-zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+# try case-sensitive match and match sub-strings
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
--- dot_config/zsh/keybindings.zsh
+++ dot_config/zsh/keybindings.zsh
@@ -1,8 +1,8 @@
 # Use vim key bindings
 bindkey -v
 
-# use 'jk' to leave command mode
-bindkey -M viins 'jk' vi-cmd-mode
+# use 'jk' to leave insert mode
+bindkey -M viins 'jk' .vi-cmd-mode
 
 # setup behavior while selection menu is open
 zmodload zsh/complist  # gives access to 'menuselect'
@@ -10,11 +10,24 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'u' undo
-bindkey -M menuselect '^M' accept-line
+bindkey -M menuselect '^M' .accept-line
 bindkey -M menuselect '^I' accept-and-infer-next-history
 
-bindkey -M vicmd 'k' fzf-history-widget
-bindkey -M vicmd 'j' expand-or-complete
-bindkey -M vicmd '^I' expand-or-complete
+# use TAB to go to menu selection
+bindkey -M vicmd '^I' down-line-or-select
+bindkey -M viins '^I' down-line-or-select
 
+# start in vi command mode
+autoload -Uz add-zle-hook-widget
+add-zle-hook-widget line-init vi-cmd-mode
+
+# fzf widget that directly runs choosen command
+fzf-history-widget-accept() {
+  fzf-history-widget
+  zle accept-line
+}
+zle     -N     fzf-history-widget-accept
+
+# use 'j' to search and run command and 'k' to be able adjust it
+bindkey -M vicmd 'j' fzf-history-widget-accept
+bindkey -M vicmd 'k' fzf-history-widget
