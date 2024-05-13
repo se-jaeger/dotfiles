28478aad90badfed075c697b99586d03ca2c2fd8
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-13 08:04:06 +0200

    use gruvbox-material theme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,19 @@
 -- change colorscheme
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
+  {
+    "f4z3r/gruvbox-material.nvim",
+    opts = {
+      contrast = "hard",
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox-material",
     },
   },
 }
