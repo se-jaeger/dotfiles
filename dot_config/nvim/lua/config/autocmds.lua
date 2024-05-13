631e63d88d3826446eb0a791ce29476e56c7ff20
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-13 08:02:29 +0200

    minor changes

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- write (i.e., only when there are changes) file when switching somewhere else (ignore errors)
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
   command = ":silent! update",
+  desc = "write (i.e., only when there are changes) file when switching somewhere else (ignore errors)",
 })
--- dot_config/nvim/lua/plugins/python.lua
+++ dot_config/nvim/lua/plugins/python.lua
@@ -2,23 +2,22 @@ return {
   -- DAP core dependencies
   { import = "lazyvim.plugins.extras.dap.core" },
 
-  -- activate LayzVim's python extras
+  -- activate LazyVim's python extras
   { import = "lazyvim.plugins.extras.lang.python" },
 
   -- disable venv because it's I'm handling this outside of nvim
   { "linux-cultist/venv-selector.nvim", enabled = false },
 
-  --  disable automatic dap installation because it skrews up my venv setup
-  --  NOTE: make sure to install debugpy in the local venv to use debugger
+  --  disable automatic dap installation because it screws up my venv setup
   { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 
-  -- override which python is use for debugging
+  -- use local venv python for debugging
   {
     "mfussenegger/nvim-dap",
     dependencies = {
+      --  NOTE: make sure to install 'debugpy' in the local venv to use debugger
       "mfussenegger/nvim-dap-python",
       config = function()
-        -- using local venv python
         require("dap-python").setup("python")
       end,
     },
