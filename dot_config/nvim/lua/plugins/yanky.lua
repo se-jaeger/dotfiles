82c3241ddfff054355cc2d20f72deb6eac5797c8
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-13 07:44:32 +0200

    fix yanky

--- dot_config/nvim/lua/plugins/yanky.lua
+++ dot_config/nvim/lua/plugins/yanky.lua
@@ -1,36 +1,24 @@
--- activate 'yanky' provided by lazyvim and use it for autocompletion
+-- activate  and cofigure 'yanky' provided by lazyvim
 return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   {
     "gbprod/yanky.nvim",
     opts = function(_, opts)
-      vim.tbl_deep_extend("force", opts, {
-        ring = {
-          history_length = 10000,
-        },
-        system_clipboard = {
-          sync_with_ring = false,
-        },
-        picker = {
-          telescope = {
-            mappings = {
-              i = {
-                -- TODO: This worked already... But now it's broken again..
-                ["<c-k>"] = require("telescope.actions").move_selection_previous,
-              },
+      opts.ring.history_length = 10000
+
+      opts.system_clipboard = {
+        sync_with_ring = false,
+      }
+
+      opts.picker = {
+        telescope = {
+          mappings = {
+            i = {
+              ["<c-k>"] = require("telescope.actions").move_selection_previous,
             },
           },
         },
-      })
+      }
     end,
-    {
-      "hrsh7th/nvim-cmp",
-      dependencies = { "chrisgrieser/cmp_yanky" },
-      opts = function(_, opts)
-        table.insert(opts.sources, {
-          name = "cmp_yanky",
-        })
-      end,
-    },
   },
 }
