e0efd6b1ad3a72497d2e2cc76a8fee6da8c2254e
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-23 23:44:12 +0200

    add missing cspell dependency

--- dot_config/nvim/lua/plugins/cspell.lua
+++ dot_config/nvim/lua/plugins/cspell.lua
@@ -1,6 +1,13 @@
 -- Use cspell for spelling
 return {
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "cspell" })
+    end,
+  },
+  {
     "mfussenegger/nvim-lint",
     opts = function(_, opts)
       opts.linters_by_ft["*"] = opts.linters_by_ft["*"] or {}
