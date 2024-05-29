27142b78da2b6233e8728df38063e22d5df56ddf
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-29 10:24:09 +0200

    add zen-mode

---
+++ dot_config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,11 @@
+return {
+  -- NOTE: alternative - https://github.com/shortcuts/no-neck-pain.nvim/wiki/Showcase#right-padding-only
+  "folke/zen-mode.nvim",
+  dependencies = {
+    {
+      "folke/twilight.nvim",
+      opts = {},
+    },
+  },
+  opts = {},
+}
