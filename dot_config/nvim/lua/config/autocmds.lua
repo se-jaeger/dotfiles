625e500700f10c9f2a92c968f659df9b81e625b2
Sebastian Jäger <git@sebastian-jaeger.me>
2024-05-29 10:23:01 +0200

    add obsidian

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
-  command = ":silent! update",
-  desc = "write (i.e., only when there are changes) file when switching somewhere else (ignore errors)",
+-- Disable built-in spellchecking for Markdown
+-- NOTE: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua#L85-L93
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("lazyvim_user_markdown", { clear = true }),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
 })
---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,53 @@
+local vault_path = vim.fn.expand("~") .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/second-brain/"
+
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  event = {
+    "BufReadPre " .. "**.md",
+    "BufNewFile " .. "**.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "second-brain",
+        path = vault_path,
+      },
+    },
+    notes_subdir = "0-inbox",
+    new_notes_location = "notes_subdir",
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
+    attachments = {
+      img_folder = "attachments",
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format("![%s](%s)", path.name, path)
+      end,
+    },
+    note_path_func = function(spec)
+      local title = ""
+      if spec.title ~= nil then
+        title = spec.title
+      else
+        title = "Untitled-"
+        for _ = 1, 4 do
+          title = title .. string.char(math.random(65, 90))
+        end
+      end
+
+      local path = spec.dir / title
+      return path:with_suffix(".md")
+    end,
+    follow_url_func = function(url)
+      vim.fn.jobstart({ "open", url })
+    end,
+  },
+}
