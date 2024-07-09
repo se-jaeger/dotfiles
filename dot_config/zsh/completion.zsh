7aed4cfd13aed88d768749e05f0f986810663d25
Sebastian Jäger <git@sebastian-jaeger.me>
2024-07-09 17:16:27 +0200

    improve zsh completion using fzf

--- dot_config/zsh/completion.zsh
+++ dot_config/zsh/completion.zsh
@@ -4,9 +4,6 @@ autoload -U compinit; compinit
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 
-# allow completion menu to be selectable
-zstyle ':completion:*' menu select
-
 # complete command options
 zstyle ':completion:*' complete-options true
 
@@ -19,11 +16,8 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-# formatting the menu
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
+# allow completion menu to be selectable
+zstyle ':completion:*' menu select
 
 # Required for completion to be in good groups (named after the tags)
 zstyle ':completion:*' group-name ''
@@ -31,3 +25,18 @@ zstyle ':completion:*' group-name ''
 # reordering listed groups
 zstyle ':completion:*:*:-command-:*:*' group-order aliases commands builtins
 zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+
+## FZF-tab settings
+zstyle ':fzf-tab:*' continuous-trigger 'tab'
+zstyle ':fzf-tab:*' fzf-bindings 'space:accept'
+zstyle ':fzf-tab:*' accept-line enter
+zstyle ':fzf-tab:*' switch-group '<' '>'
+
+## Previews
+# command: tldr
+zstyle ':fzf-tab:complete:tldr:argument-1' fzf-preview \
+    'tldr --color always $word'
+
+# command: man
+zstyle ':fzf-tab:complete:(\\|*/|)man:' fzf-preview \
+    '[[ $group == "manual page" ]] && MANPAGER="" man $word | bat --color always --language man'
--- dot_config/zsh/keybindings.zsh
+++ dot_config/zsh/keybindings.zsh
@@ -4,31 +4,13 @@ bindkey -v
 # use 'jk' to leave insert mode
 bindkey -M viins 'jk' .vi-cmd-mode
 
-# setup behavior while selection menu is open
-zmodload zsh/complist  # gives access to 'menuselect'
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'u' undo
-bindkey -M menuselect '^M' .accept-line  # NOTE: leading '.' is important here!
-bindkey -M menuselect '^I' .expand-or-complete-prefix
-
-# use TAB to go to menu selection
-bindkey -M vicmd '^I' menu-expand-or-complete
-bindkey -M viins '^I' menu-expand-or-complete
-
-# start in vi command mode
-autoload -Uz add-zle-hook-widget
-add-zle-hook-widget line-init vi-cmd-mode
-
-# fzf widget that directly runs choosen command
+# fzf widget that directly runs chosen command
 fzf-history-widget-accept() {
   fzf-history-widget
   zle accept-line
 }
 zle     -N     fzf-history-widget-accept
 
-# use 'j' to search and run command and 'k' to be able adjust it
-bindkey -M vicmd 'j' fzf-history-widget-accept
-bindkey -M vicmd 'k' fzf-history-widget
+# use 'CTRL-J' and 'CTRL-k' to search the history
+bindkey '^J' fzf-history-widget-accept
+bindkey '^K' fzf-history-widget
