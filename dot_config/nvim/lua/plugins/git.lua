0a79af04e0222f2a638c7656cbb8f080dda6899b
Sebastian Jäger <git@sebastian-jaeger.me>
2024-07-09 17:19:59 +0200

    setup diffview

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -5,13 +5,21 @@ return {
     cmd = { "AdvancedGitSearch" },
     dependencies = {
       "nvim-telescope/telescope.nvim",
+      "sindrets/diffview.nvim",
     },
     config = function()
+      require("telescope").setup({
+        extensions = {
+          advanced_git_search = {
+            diff_plugin = "diffview",
+          },
+        },
+      })
       require("telescope").load_extension("advanced_git_search")
     end,
-    -- TODO: add other mappings and setup <CR> to open diff somehow...
     keys = {
       { "<leader>gf", ":AdvancedGitSearch search_log_content_file<CR>", desc = "Current File History" },
+      { "<leader>gF", ":AdvancedGitSearch search_log_content<CR>", desc = "Search Git History for File Content" },
     },
   },
 
