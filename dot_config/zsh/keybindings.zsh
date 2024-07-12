c0d84c50ad6e7420916ceedd25c201c01091dfa8
Sebastian Jäger <git@sebastian-jaeger.me>
2024-07-12 11:17:20 +0200

    add zsh keybinding for CTRL-H/CTRL-L to move cursor

--- dot_config/zsh/keybindings.zsh
+++ dot_config/zsh/keybindings.zsh
@@ -14,3 +14,7 @@ zle     -N     fzf-history-widget-accept
 # use 'CTRL-J' and 'CTRL-k' to search the history
 bindkey '^J' fzf-history-widget-accept
 bindkey '^K' fzf-history-widget
+
+# use 'CTRL-H' and 'CTRL-L' to move chars backward/forward
+bindkey -M viins '^H' vi-backward-char
+bindkey -M viins '^L' vi-forward-char
