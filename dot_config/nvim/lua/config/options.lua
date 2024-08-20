a5805309e8f8654d4bde17c07057c077ea887abe
Sebastian Jäger <git@sebastian-jaeger.me>
2024-08-20 12:01:57 +0200

    remove temporary fix

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -32,22 +32,3 @@ opt.spell = true
 
 -- do not use system clipboard
 opt.clipboard = ""
-
--- TODO: This is a workaround: https://github.com/folke/which-key.nvim/issues/584
-if vim.env.SSH_TTY then
-  local function paste()
-    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-  end
-  local osc52 = require("vim.ui.clipboard.osc52")
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = osc52.copy("+"),
-      ["*"] = osc52.copy("*"),
-    },
-    paste = {
-      ["+"] = paste,
-      ["*"] = paste,
-    },
-  }
-end
