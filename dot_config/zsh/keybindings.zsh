6f9e7cbab9fa45dd2796175a60f0638b0ca7c79a
Sebastian Jäger <git@sebastian-jaeger.me>
2024-08-25 15:55:06 +0200

    using ^K in zsh activates cmd mode

--- dot_config/zsh/keybindings.zsh
+++ dot_config/zsh/keybindings.zsh
@@ -2,18 +2,25 @@
 bindkey -v
 
 # use 'jk' to leave insert mode
-bindkey -M viins 'jk' .vi-cmd-mode
+bindkey -M viins 'jk' vi-cmd-mode
 
-# fzf widget that directly runs chosen command
+# some custom fzf widgets
 fzf-history-widget-accept() {
   fzf-history-widget
   zle accept-line
 }
-zle     -N     fzf-history-widget-accept
+
+fzf-history-widget-command-mode() {
+  fzf-history-widget
+  zle vi-cmd-mode
+}
+
+zle -N fzf-history-widget-accept
+zle -N fzf-history-widget-command-mode
 
 # use 'CTRL-J' and 'CTRL-k' to search the history
 bindkey '^J' fzf-history-widget-accept
-bindkey '^K' fzf-history-widget
+bindkey '^K' fzf-history-widget-command-mode
 
 # use 'CTRL-H' and 'CTRL-L' to move chars backward/forward
 bindkey -M viins '^H' vi-backward-char
