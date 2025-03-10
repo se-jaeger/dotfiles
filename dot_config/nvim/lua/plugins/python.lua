a8c9f5b0f0245cb875f3eecd510293448789782c
Sebastian Jäger <git@sebastian-jaeger.me>
2025-03-10 10:36:54 +0100

    use mypy/ruff from system or package only

--- dot_config/nvim/lua/plugins/python.lua
+++ dot_config/nvim/lua/plugins/python.lua
@@ -25,21 +25,6 @@ return {
 
   -- setup linting/formatting
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "mypy", "ruff" })
-    end,
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        python = { "mypy" },
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
