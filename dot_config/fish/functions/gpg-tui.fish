8605fd0a05333efbdd973348141fb221e6d4ebed
Sebastian Jäger <github@sebastian-jaeger.me>
2025-04-19 21:46:48 +0200

    minor: fix function name

--- dot_config/fish/functions/gpg-tui.fish
+++ dot_config/fish/functions/gpg-tui.fish
@@ -1,3 +1,3 @@
-function gpg-tui gpg-tui --description "alias gpg-tui -> gpg-tui --config '~/.config/gpg-tui/gpg-tui.toml'"
+function gpg-tui --description "alias gpg-tui -> gpg-tui --config '~/.config/gpg-tui/gpg-tui.toml'"
     command gpg-tui --config '~/.config/gpg-tui/gpg-tui.toml' $argv
 end
