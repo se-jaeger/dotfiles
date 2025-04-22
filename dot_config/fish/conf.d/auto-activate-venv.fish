5eb5945b1d9c8e9615f870613faa62f378fc286c
Sebastian Jäger <github@sebastian-jaeger.me>
2025-04-22 21:35:14 +0200

    misc changes/improvements

--- dot_config/fish/conf.d/auto-activate-venv.fish
+++ dot_config/fish/conf.d/auto-activate-venv.fish
@@ -1,43 +1,46 @@
-# Global flag to track if we're in the middle of handling venv
-set --global __VENV_HANDLING 0
+if status is-interactive
 
-function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-    # Prevent running during command substitution or if we're already handling venv
-    status --is-command-substitution; and return
-    test "$__VENV_HANDLING" -eq 1; and return
-
-    set --global __VENV_HANDLING 1
-
-    # Check if we are inside a git repository
-    if command git rev-parse --show-toplevel &>/dev/null
-        set dir (realpath (command git rev-parse --show-toplevel))
-    else
-        set dir (pwd)
-    end
-
-    # Find a virtual environment in the directory
-    set --local VENV_DIR_NAMES env .env venv .venv
-    set --local venv_dir ""
-    for name in $VENV_DIR_NAMES
-        if test -e "$dir/$name/bin/activate.fish"
-            set venv_dir "$dir/$name"
-            break
-        end
-    end
-
-    if test -n "$venv_dir" -a "$VIRTUAL_ENV" != "$venv_dir" -a -e "$venv_dir/bin/activate.fish"
-        # prompt is handled by starship
-        set --local --export VIRTUAL_ENV_DISABLE_PROMPT 1
-        source "$venv_dir/bin/activate.fish"
-
-    else if test -n "$VIRTUAL_ENV" -a -z "$venv_dir"
-        deactivate
-    end
-
+    # Global flag to track if we're in the middle of handling venv
     set --global __VENV_HANDLING 0
-end
 
-# Only run initial check if we're not already handling venv
-if test "$__VENV_HANDLING" -eq 0
-    __auto_source_venv
+    function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
+        # Prevent running during command substitution or if we're already handling venv
+        status --is-command-substitution; and return
+        test "$__VENV_HANDLING" -eq 1; and return
+
+        set --global __VENV_HANDLING 1
+
+        # Check if we are inside a git repository
+        if command git rev-parse --show-toplevel &>/dev/null
+            set dir (realpath (command git rev-parse --show-toplevel))
+        else
+            set dir (pwd)
+        end
+
+        # Find a virtual environment in the directory
+        set --local VENV_DIR_NAMES env .env venv .venv
+        set --local venv_dir ""
+        for name in $VENV_DIR_NAMES
+            if test -e "$dir/$name/bin/activate.fish"
+                set venv_dir "$dir/$name"
+                break
+            end
+        end
+
+        if test -n "$venv_dir" -a "$VIRTUAL_ENV" != "$venv_dir" -a -e "$venv_dir/bin/activate.fish"
+            # prompt is handled by starship
+            set --local --export VIRTUAL_ENV_DISABLE_PROMPT 1
+            source "$venv_dir/bin/activate.fish"
+
+        else if test -n "$VIRTUAL_ENV" -a -z "$venv_dir"
+            deactivate
+        end
+
+        set --global __VENV_HANDLING 0
+    end
+
+    # Only run initial check if we're not already handling venv
+    if test "$__VENV_HANDLING" -eq 0
+        __auto_source_venv
+    end
 end
--- dot_config/fish/conf.d/ls-on-changed-PWD.fish
+++ dot_config/fish/conf.d/ls-on-changed-PWD.fish
@@ -1,5 +1,7 @@
-function ls_on_changed_PWD --on-variable PWD --description "List Directory when PWD changed"
-    # Prevent running during command substitution
-    status --is-command-substitution; and return
-    l
+if status is-interactive
+    function ls_on_changed_PWD --on-variable PWD --description "List Directory when PWD changed"
+        # Prevent running during command substitution
+        status --is-command-substitution; and return
+        l
+    end
 end
