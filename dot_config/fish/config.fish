6979907e488ad479831450b238d4835c6d80d157
Sebastian Jäger <github@sebastian-jaeger.me>
2025-04-24 09:38:33 +0200

    add harper-ls language server setup

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -32,11 +32,6 @@ if type --query bat
     set --global --export MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 end
 
-# rust 
-if test -f "$HOME/.cargo/env.fish"
-    . "$HOME/.cargo/env.fish"
-end
-
 # fzf
 set --global --export FZF_DEFAULT_COMMAND "fd --follow"
 set --global --export FZF_DEFAULT_OPTS "
