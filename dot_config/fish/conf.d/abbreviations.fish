711403add131f5d9c1b43de7cfc5a418d170261b
Sebastian Jäger <github@sebastian-jaeger.me>
2025-05-15 16:57:10 +0200

    split abbreviations and keybindings and remove some unused once.

---
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    #### Abbreviations ####
+    #######################
+
+    abbr --position command b brew
+    abbr --position command k kubectl
+    abbr --position command o open
+    abbr --position command c clear
+    abbr --position command jf fj
+    abbr --position command cat bat
+
+    # Jujutsu
+    abbr --position command je jj edit
+    abbr --position command jdf jj diff
+    abbr --position command jds jj describe
+    abbr --position command jl jj log
+    abbr --position command jn jj new
+    abbr --position command jsh jj show
+    abbr --position command jsp jj split -i
+    abbr --position command jsq jj squash
+    abbr --position command jst jj status
+end
---
+++ dot_config/fish/conf.d/keybindings.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    ####### Key Bindigns #######
+    ############################
+
+    # using vi-bindings
+    fish_vi_key_bindings
+
+    for mode in default insert
+        bind --mode $mode alt-backspace backward-kill-word
+
+        bind --mode $mode ctrl-f fzf-file-widget
+        bind --mode $mode tab _fzf_search_completions
+
+        bind --mode $mode ctrl-j _fzf_search_history execute
+        bind --mode $mode ctrl-k _fzf_search_history 'set fish_bind_mode default'
+    end
+
+    bind --mode default U redo
+end
