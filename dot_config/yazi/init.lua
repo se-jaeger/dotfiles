0a3d788351d3a92f902f3645840a2bbb3f8074cd
Sebastian Jäger <git@sebastian-jaeger.me>
2026-03-01 17:32:55 +0100

    update/upgrade yazi
    Signed-off-by: Sebastian Jäger <git@sebastian-jaeger.me>

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,15 +1,2 @@
 require("full-border"):setup()
-require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, only_motions = true })
 require("starship"):setup()
-require("git"):setup()
-require("augment-command"):setup({
-	prompt = false,
-	default_item_group_for_prompt = "hovered",
-	smart_enter = true,
-	smart_paste = false,
-	enter_archives = true,
-	must_have_hovered_item = true,
-	skip_single_subdirectory_on_enter = true,
-	skip_single_subdirectory_on_leave = true,
-	wraparound_file_navigation = false,
-})
